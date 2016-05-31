ed1596f463bf41fc602a709fd787eacfe0374bdb
giggles2710 <giggles.2710@gmail.com>
2016-05-31 16:51:03 +0700

    ThuanNH - Update DB

---
+++ Utility/EncryptionHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class EncryptionHelper
+    {
+        private static string SecretKey = "9TZTi6PdCcG0JurXNjH6ww==";
+        private static string IvKey = "TZJbkD968uwJ2ZVEDmyPiw==";
+
+        public static byte[] Encrypt(string value, byte[] key, byte[] iv)
+        {
+            byte[] encrypted;
+            using (var rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = key;
+                rijAlg.IV = iv;
+
+                // Create a decrytor to perform the stream transform.
+                var encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(value);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return encrypted;
+        }
+
+        public static string HashPassword(string password, string saltToken)
+        {
+            var hmacSHA512 = new HMACSHA512(System.Text.Encoding.UTF8.GetBytes(saltToken));
+            var saltedHash = hmacSHA512.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            return Convert.ToBase64String(saltedHash);
+        }
+    }
+}
---
+++ Utility/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class UtilityHelper
+    {
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomTelephoneNumber()
+        {
+            var rand = new Random();
+            StringBuilder telNo = new StringBuilder(12);
+            int number;
+            for (int i = 0; i < 3; i++)
+            {
+                number = rand.Next(0, 8); // digit between 0 (incl) and 8 (excl)
+                telNo = telNo.Append(number.ToString());
+            }
+            telNo = telNo.Append("-");
+            number = rand.Next(0, 743); // number between 0 (incl) and 743 (excl)
+            telNo = telNo.Append(String.Format("{0:D3}", number));
+            telNo = telNo.Append("-");
+            number = rand.Next(0, 10000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D4}", number));
+
+            return telNo.ToString();
+        }
+    }
+}
